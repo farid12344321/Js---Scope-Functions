4e7b5cf9d922310b0ab4eacd669f7789d7fc0f53
Faridrab <faridrab@code.edu.az>
2023-02-08 02:25:45 +0400

    js homework

---
+++ assets/css/script.js
@@ -0,0 +1,113 @@
+//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+// function dividedOrNot(n){
+//     if(n%3 ==0 && n%7==0){
+//         console.log("bolunur")
+//     }
+//     else{
+//         console.log("bolunmur")
+//     }
+// }
+
+// dividedOrNot(21)
+
+
+
+
+
+
+
+
+//2) Functiona musbet bir eded gelir, hemin funksiya gelen ededin faktorialini hesablayib geri qaytarmalidir. 
+
+// function facctorial(n){
+//     multiple = 1;
+    
+    
+//     for (let i = 1; i <= n; i++)
+//     {
+//         multiple *= i;
+//     }
+//         console.log(multiple);
+// }
+// facctorial(5)
+
+
+
+
+
+
+
+//3) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini 
+//hesablayan bir function yazin.
+
+
+
+// function account(mail,password){
+//    if(mail =="cavid@code.edu.az" && password =="12345" ){
+//     console.log("girish olundu")
+//    }
+//    else{
+//     console.log("mail ve ya password sehfdir")
+//    }
+// }
+
+
+// account("cavid@code.edu.az" ,"12345");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//5) Functiona ededlerden ibaret array gelir, function hemin arraydaki tek ededlerin cemini qaytarmalidir.
+
+// function oddnNumbers(arr) {
+
+//     sum=0;
+    
+//     for (let i = 0; i < arr.length; i++) {
+     
+//         if (arr[i] % 2 == 1) {
+//              sum+=arr[i]
+//         }
+//     }
+//     console.log(sum)
+// }
+// nums=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+// oddnNumbers(nums);
+
+
+
+
+
+
+
+
+
+
+//6) Functiona ededlerden ibaret array gelir, function hemin arraydaki cut ededlerin sayini qaytarmalidir.
+
+// function evenNumbers(arr) {
+
+//     count=0;
+    
+//     for (let i = 0; i < arr.length; i++) {
+     
+//         if (arr[i] % 2 == 0) {
+//             count++;
+//         }
+//     }
+//     console.log(count)
+// }
+// nums=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+//  evenNumbers(nums);
+
+
